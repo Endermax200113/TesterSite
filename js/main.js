1c092d730030855a4bdbe249bd55e8b0c30d6825
Endermax <Endermax200113@mail.ru>
2019-11-22 00:15:57 +0300

    Разработано 1-ый контент сайта

--- js/main.js
+++ js/main.js
@@ -1,3 +1,32 @@
+// -> Переменные
 //ОСНОВА
-var numberTest = 8;
+var numberTest = 9;
 var test = "[Тест №" + numberTest + "] Это тестовый сайт!";
+
+//ШАПКА
+//Основы размеров
+var headerBgMainHeight = window.getComputedStyle(document.querySelector(".headerBgMain")).getPropertyValue("height");
+var headerMenuAllHeight = window.getComputedStyle(document.querySelector(".headerMenuAll")).getPropertyValue("height");
+var headerBasicHeight = window.getComputedStyle(document.querySelector(".headerBasic")).getPropertyValue("height");
+
+//Специальная пустота
+var headerEmpty = document.querySelector("#headerEmpty");
+
+// -! Выполнение скрипта при загрузки страницы !-
+startScript();
+
+// -> Слушатели
+// -! Во время изменения окна !-
+window.onresize = function(e) {
+	headerBgMainHeight = window.getComputedStyle(document.querySelector(".headerBgMain")).getPropertyValue("height");
+	headerMenuAllHeight = window.getComputedStyle(document.querySelector(".headerMenuAll")).getPropertyValue("height");
+	headerBasicHeight = window.getComputedStyle(document.querySelector(".headerBasic")).getPropertyValue("height");
+
+	headerEmpty.style = "height: calc(" + headerBgMainHeight + " - " + headerMenuAllHeight + " - " + headerBasicHeight + " - 75px);";
+}
+
+// -> Функции
+//ОСНОВА
+function startScript() {
+	headerEmpty.style = "height: calc(" + headerBgMainHeight + " - " + headerMenuAllHeight + " - " + headerBasicHeight + " - 75px);";
+}
