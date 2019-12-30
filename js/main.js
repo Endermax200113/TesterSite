769a9d7be41624fee7a4ec49c8147d3df87228f2
Endermax <Endermax200113@mail.ru>
2019-12-30 14:32:09 +0300

    Адаптация для моб версии

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var test = 1;
+var test = 2;
 
 var page = 1;
 var page1 = fromElement("#page1");
@@ -30,7 +30,9 @@ document.addEventListener("click", function(e) {
 	} else if (equalsElements(element, page2)) {
 		if (page == 2) return;
 
-		setStyleEl(blocks, "margin-left", "-700px");
+		if (docWidth > 768) setStyleEl(blocks, "margin-left", "-700px");
+		else if (docWidth > 425) setStyleEl(blocks, "margin-left", "-350px");
+		else if (docWidth >= 320) setStyleEl(blocks, "margin-left", "-250px");
 		if (page == 1) renameClassEl(page1, "contentPagesHere", "contentPagesNot");
 		else if (page == 3) renameClassEl(page3, "contentPagesHere", "contentPagesNot");
 		renameClassEl(page2, "contentPagesNot", "contentPagesHere");
@@ -41,7 +43,9 @@ document.addEventListener("click", function(e) {
 	} else if (equalsElements(element, page3)) {
 		if (page == 3) return;
 
-		setStyleEl(blocks, "margin-left", "-1400px");
+		if (docWidth > 768) setStyleEl(blocks, "margin-left", "-1400px");
+		else if (docWidth > 425) setStyleEl(blocks, "margin-left", "-700px");
+		else if (docWidth >= 320) setStyleEl(blocks, "margin-left", "-500px");
 		if (page == 1) renameClassEl(page1, "contentPagesHere", "contentPagesNot");
 		else if (page == 2) renameClassEl(page2, "contentPagesHere", "contentPagesNot");
 		renameClassEl(page3, "contentPagesNot", "contentPagesHere");
@@ -53,7 +57,9 @@ document.addEventListener("click", function(e) {
 		if (page == 1) return;
 
 		if (page == 3) {
-			setStyleEl(blocks, "margin-left", "-700px");
+			if (docWidth > 768) setStyleEl(blocks, "margin-left", "-700px");
+			else if (docWidth > 425) setStyleEl(blocks, "margin-left", "-350px");
+			else if (docWidth >= 320) setStyleEl(blocks, "margin-left", "-250px");
 			renameClassEl(page3, "contentPagesHere", "contentPagesNot");
 			renameClassEl(page2, "contentPagesNot", "contentPagesHere");
 			setStyleEl(rightArrow, "opacity", "1");
@@ -69,17 +75,37 @@ document.addEventListener("click", function(e) {
 		if (page == 3) return;
 
 		if (page == 1) {
-			setStyleEl(blocks, "margin-left", "-700px");
+			if (docWidth > 768) setStyleEl(blocks, "margin-left", "-700px");
+			else if (docWidth > 425) setStyleEl(blocks, "margin-left", "-350px");
+			else if (docWidth >= 320) setStyleEl(blocks, "margin-left", "-250px");
 			renameClassEl(page1, "contentPagesHere", "contentPagesNot");
 			renameClassEl(page2, "contentPagesNot", "contentPagesHere");
 			setStyleEl(leftArrow, "opacity", "1");
 		} else if (page == 2) {
-			setStyleEl(blocks, "margin-left", "-1400px");
+			if (docWidth > 768) setStyleEl(blocks, "margin-left", "-1400px");
+			else if (docWidth > 425) setStyleEl(blocks, "margin-left", "-700px");
+			else if (docWidth >= 320) setStyleEl(blocks, "margin-left", "-500px");
 			renameClassEl(page2, "contentPagesHere", "contentPagesNot");
 			renameClassEl(page3, "contentPagesNot", "contentPagesHere");
 			setStyleEl(rightArrow, "opacity", "0");
 		}
 
 		page++;
+	}
+});
+
+window.addEventListener("resize", function(e) {
+	if (docWidth > 768) {
+		if (page == 1) return;
+		else if (page == 2) setStyleEl(blocks, "margin-left", "-700px");
+		else if (page == 3) setStyleEl(blocks, "margin-left", "-1400px");
+	} else if (docWidth > 425) {
+		if (page == 1) return;
+		else if (page == 2) setStyleEl(blocks, "margin-left", "-350px");
+		else if (page == 3) setStyleEl(blocks, "margin-left", "-700px");
+	} else if (docWidth >= 320) {
+		if (page == 1) return;
+		else if (page == 2) setStyleEl(blocks, "margin-left", "-250px");
+		else if (page == 3) setStyleEl(blocks, "margin-left", "-500px");
 	}
 });
