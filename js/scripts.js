007a253e35abd82d8f0e0d6504d18f05669ae02d
Endermax200113 <47977493+Endermax200113@users.noreply.github.com>
2020-05-11 02:24:41 +0300

    Создание нового сайта, добавление хедера

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+//Нужный скрипт
+
+var docWidth = window.innerWidth;
+var docHeight = window.innerHeight;
+var deviceMobile = docWidth <= 425 && docWidth >= 320;
+var deviceTablet = docWidth <= 768 && docWidth > 425;
+var deviceLaptop = docWidth <= 1024 && docWidth > 768;
+var deviceComp = docWidth > 1024;
+
+$(document).ready(() => {
+	$(window).bind("resize", () => {
+		docWidth = window.innerWidth;
+		docHeight = window.innerHeight;
+		deviceMobile = docWidth <= 425 && docWidth >= 320;
+		deviceTablet = docWidth <= 768 && docWidth > 425;
+		deviceLaptop = docWidth <= 1024 && docWidth > 768;
+		deviceComp = docWidth > 1024;
+	});
+});
+
+function testFunction() {
+	alert("Функция успешно работает!");
+}
+
+
+//Главный скрипт
+
+var version = "0.0.0";
+var copyrite = "© PskovPages";
+
+$(document).ready(() => {
+	console.log("Версия сайта: " + version);
+	console.log(copyrite);
+});
