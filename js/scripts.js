4d920bdf973629a96c858897a5886f5721b078ab
Endermax200113 <47977493+Endermax200113@users.noreply.github.com>
2020-05-20 18:46:46 +0300

    Прикреплён хедер

--- js/scripts.js
+++ js/scripts.js
@@ -2,19 +2,26 @@
 
 var docWidth = window.innerWidth;
 var docHeight = window.innerHeight;
-var deviceMobile = docWidth <= 425 && docWidth >= 320;
-var deviceTablet = docWidth <= 768 && docWidth > 425;
-var deviceLaptop = docWidth <= 1024 && docWidth > 768;
-var deviceComp = docWidth > 1024;
+var deviceMobile = docWidth <= 480 && docWidth >= 320;
+var deviceTablet = docWidth <= 768 && docWidth > 480;
+var deviceLaptop = docWidth <= 1200 && docWidth > 768;
+var deviceComp = docWidth <= 1440 && docWidth > 1200;
+var device2K = docWidth <= 2560 && docWidth > 1440;
+var device4K = docWidth <= 4096 && docWidth > 2560;
+var deviceBig = docWidth > 4096;
 
 $(document).ready(() => {
 	$(window).bind("resize", () => {
 		docWidth = window.innerWidth;
 		docHeight = window.innerHeight;
-		deviceMobile = docWidth <= 425 && docWidth >= 320;
-		deviceTablet = docWidth <= 768 && docWidth > 425;
-		deviceLaptop = docWidth <= 1024 && docWidth > 768;
-		deviceComp = docWidth > 1024;
+		
+		deviceMobile = docWidth <= 480 && docWidth >= 320;
+		deviceTablet = docWidth <= 768 && docWidth > 480;
+		deviceLaptop = docWidth <= 1200 && docWidth > 768;
+		deviceComp = docWidth <= 1440 && docWidth > 1200;
+		device2K = docWidth <= 2560 && docWidth > 1440;
+		device4K = docWidth <= 4096 && docWidth > 2560;
+		deviceBig = docWidth > 4096;
 	});
 });
 
@@ -25,7 +32,7 @@ function testFunction() {
 
 //Главный скрипт
 
-var version = "0.0.16";
+var version = "0.0.17";
 var copyrite = "© PskovPages";
 
 $(document).ready(() => {
@@ -46,6 +53,10 @@ var animate = $(".header-main-mob-menu-animate");
 var link1 = $(".link-one");
 var link2 = $(".link-two");
 var link3 = $(".link-three");
+var links = $(".header-links");
+var cloneLinks = links.clone();
+var header = $(".header");
+var cloneHeader = $(".header-empty");
 var actived = false;
 
 icon.on("click", (e) => {
@@ -80,6 +91,58 @@ icon.on("click", (e) => {
 	}
 });
 
+links.before(cloneLinks);
+links.addClass('fixed');
+
+$(window).ready((e) => {
+	if (deviceMobile) {
+		header.css('position', 'fixed');
+		header.css('z-index', '40');
+		cloneHeader.css('height', '' + header.height());
+	} else {
+		if (deviceLaptop) links.css('padding', '20px 0px');
+		else if (device2K) links.css('padding', '25px 0px');
+		else if (device4K || deviceBig) links.css('padding', '75px 0px');
+	}
+});
+
+$(window).resize((e) => {
+	if (deviceMobile) {
+		header.css('position', 'fixed');
+		header.css('z-index', '40');
+		cloneHeader.css('height', '' + header.height());
+	} else {
+		header.css('position', 'relative');
+		cloneHeader.css('height', '0');
+		$(".fixed-show").css('z-index', '50');
+
+		if (deviceLaptop) links.css('padding', '20px 0px');
+		else if (device2K) links.css('padding', '25px 0px');
+		else if (device4K || deviceBig) links.css('padding', '75px 0px');
+	}
+});
+
+$(window).scroll((e) => {
+	console.log($(window).scrollTop());
+
+	if ($(window).scrollTop() > getScrollPx()) {
+		links.addClass('fixed-show');
+		links.removeClass('fixed');
+	} else {
+		links.removeClass('fixed-show');
+		links.addClass('fixed');
+	}
+});
+
 function zIndex() {
 	if (!actived) menu.css("z-index", "-1");
+}
+
+function getScrollPx() {
+	if (deviceMobile) return 0;
+	else if (deviceTablet) return 115;
+	else if (deviceLaptop) return 160;
+	else if (deviceComp) return 200;
+	else if (device2K) return 290;
+	else return 470;
 }
